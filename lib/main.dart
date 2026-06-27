34043a2fc01c5ab58baa5f9c9d66d09a81857438
Ernesto Rangel <ernestojrcjunior@gmail.com>
2026-06-26 21:14:49 -0300

    feat: update main screen design and adjust asset configuration

--- lib/main.dart
+++ lib/main.dart
@@ -6,7 +6,7 @@ void main() {
       home: Scaffold(
         body: Container(
           decoration: BoxDecoration(
-            color: Colors.deepPurple,
+            color: const Color.fromARGB(255, 52, 20, 106),
           ),
           child: Center(
             child: Column(
@@ -14,16 +14,24 @@ void main() {
               children: [
                 Image.asset(
                   'assets/images/quiz-logo.png',
-                  width: 200,
+                  width: 300,
                 ),
-                const SizedBox(height: 20),
-                const Text('Learn Flutter the fun way!'),
-                const SizedBox(height: 20),
+                const SizedBox(height: 60),
+                const Text(
+                  'Learn Flutter the fun way!',
+                  style: TextStyle(
+                    fontSize: 22,
+                    color: Colors.white,
+                  ),
+                ),
+                const SizedBox(height: 40),
                 OutlinedButton(
                   onPressed: null,
                   style: OutlinedButton.styleFrom(
                     foregroundColor: Colors.white,
-                    textStyle: TextStyle(fontSize: 28),
+                    textStyle: TextStyle(
+                      fontSize: 14,
+                    ),
                   ),
                   child: const Text('Start Quiz'),
                 ),
