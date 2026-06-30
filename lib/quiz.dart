c1cf5baae8d3134ed2668b621271f2a0f501d40f
Ernesto Rangel <ernestojrcjunior@gmail.com>
2026-06-30 18:52:21 -0300

    feat: refactor Quiz and StartScreen to improve layout and screen switching

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -11,11 +12,24 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
+  Widget activeScreen = const StartScreen();
+
+  void switchScreen() {
+    setState(() {
+      activeScreen = const QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(context) {
     return MaterialApp(
       home: Scaffold(
-        body: StartScreen(),
+        body: Container(
+          decoration: BoxDecoration(
+            color: const Color.fromARGB(255, 52, 20, 106),
+          ),
+          child: activeScreen,
+        ),
       ),
     );
   }
--- lib/start_screen.dart
+++ lib/start_screen.dart
@@ -5,41 +5,36 @@ class StartScreen extends StatelessWidget {
 
   @override
   Widget build(context) {
-    return Container(
-      decoration: BoxDecoration(
-        color: const Color.fromARGB(255, 52, 20, 106),
-      ),
-      child: Center(
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          children: [
-            Image.asset(
-              'assets/images/quiz-logo.png',
-              width: 300,
-              color: const Color.fromARGB(150, 255, 255, 255),
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 60),
+          const Text(
+            'Learn Flutter the fun way!',
+            style: TextStyle(
+              fontSize: 22,
+              color: Colors.white,
             ),
-            const SizedBox(height: 60),
-            const Text(
-              'Learn Flutter the fun way!',
-              style: TextStyle(
-                fontSize: 22,
-                color: Colors.white,
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+            onPressed: () {},
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.white,
+              textStyle: TextStyle(
+                fontSize: 14,
               ),
             ),
-            const SizedBox(height: 30),
-            OutlinedButton.icon(
-              onPressed: () {},
-              style: OutlinedButton.styleFrom(
-                foregroundColor: Colors.white,
-                textStyle: TextStyle(
-                  fontSize: 14,
-                ),
-              ),
-              icon: const Icon(Icons.arrow_right_alt),
-              label: const Text('Start Quiz'),
-            ),
-          ],
-        ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          ),
+        ],
       ),
     );
   }
